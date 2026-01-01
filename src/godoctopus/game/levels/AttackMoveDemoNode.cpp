958e0829af014fb6de474b297475c5e9418e96c3
SimonP <SimonP@gmail.com>
2026-01-01 13:03:30 +0100

    better custom projectile object

--- src/godoctopus/game/levels/AttackMoveDemoNode.cpp
+++ src/godoctopus/game/levels/AttackMoveDemoNode.cpp
@@ -24,8 +24,13 @@
 
 namespace godot {
 
-struct CustomProj {};
-struct ProjTrajectory {
+struct CustomBasicProjectile {};
+struct CustomBasicProjectileInfo {
+	int64_t r;
+	int64_t g;
+	int64_t b;
+};
+struct ProjectileTrajectory {
 	octopus::Fixed origin_y;
 	octopus::Fixed target_y;
 };
@@ -35,26 +40,41 @@ void AttackMoveDemoNode::setup(Dictionary const &meta_data, GameNode &game) {
 
 	flecs::world &ecs = game.get_world().ecs;
 
-	ecs.component<CustomProj>();
-	ecs.component<ProjTrajectory>()
-		.member("origin_y", &ProjTrajectory::origin_y)
-		.member("target_y", &ProjTrajectory::target_y)
+	ecs.component<CustomBasicProjectile>();
+	ecs.component<CustomBasicProjectileInfo>()
+		.member("r", &CustomBasicProjectileInfo::r)
+		.member("g", &CustomBasicProjectileInfo::g)
+		.member("b", &CustomBasicProjectileInfo::b)
+	;
+	ecs.component<ProjectileTrajectory>()
+		.member("origin_y", &ProjectileTrajectory::origin_y)
+		.member("target_y", &ProjectileTrajectory::target_y)
+	;
+	ecs.component<octopus::BasicProjectileAttack<CustomBasicProjectile>>()
+		.member("speed", &octopus::BasicProjectileAttack<CustomBasicProjectile>::speed)
 	;
 
-	octopus::set_up_basic_projectile_systems<CustomProj>(ecs);
+	octopus::set_up_basic_projectile_systems<CustomBasicProjectile>(ecs);
 
-	ecs.observer<octopus::Projectile const, octopus::ProjectileConstants const, CustomProj const>()
+	ecs.observer<octopus::Projectile const, octopus::ProjectileConstants const, CustomBasicProjectile const>()
 			.event(flecs::OnAdd)
-			.each([] (flecs::entity e, octopus::Projectile const& proj, octopus::ProjectileConstants const& cst, CustomProj const &) {
+			.each([] (flecs::entity e, octopus::Projectile const& proj, octopus::ProjectileConstants const& cst, CustomBasicProjectile const &) {
 				octopus::Fixed up = 1.5;
 				octopus::Fixed end_up = 0.25;
-				if (e && e.try_get<ProjTrajectory>()) {
-					up = e.try_get<ProjTrajectory>()->origin_y;
+				if (e && e.try_get<ProjectileTrajectory>()) {
+					up = e.try_get<ProjectileTrajectory>()->origin_y;
 				}
-				if (proj.target && proj.target.try_get<ProjTrajectory>()) {
-					end_up = proj.target.try_get<ProjTrajectory>()->target_y;
+				if (proj.target && proj.target.try_get<ProjectileTrajectory>()) {
+					end_up = proj.target.try_get<ProjectileTrajectory>()->target_y;
 				}
-				e.set<SmartMMeshLibraryHandle>({0,22./256, 90./256, 76./256,1.,0.2,
+				float r=22./256, g=90./256, b=76./256;
+				auto info = e.try_get<CustomBasicProjectileInfo>();
+				if (e && info) {
+					r = info->r / 256.;
+					g = info->g / 256.;
+					b = info->b / 256.;
+				}
+				e.set<SmartMMeshLibraryHandle>({0, r, g, b, 1., 0.2,
 					up, // up
 					end_up, // end_up
 					octopus::get_time_stamp(e.world()),
@@ -65,7 +85,7 @@ void AttackMoveDemoNode::setup(Dictionary const &meta_data, GameNode &game) {
 	// pop damage
 	ecs.system<octopus::ProjectileTrigger const, octopus::Projectile const, octopus::Position const, SmartMMeshLibraryHandle const>()
 		.kind(ecs.entity(EndUpdatePhase))
-		.with<CustomProj>()
+		.with<CustomBasicProjectile>()
 		.each([this](flecs::entity e, octopus::ProjectileTrigger const&, octopus::Projectile const &,
 				octopus::Position const &pos, SmartMMeshLibraryHandle const &handle) {
 			if (_particules) {
@@ -83,12 +103,12 @@ void AttackMoveDemoNode::setup(Dictionary const &meta_data, GameNode &game) {
 			.add<octopus::PositionInTree>()
 			.set<octopus::Team>({1})
 			.add<octopus::Destroyable>()
-			.set<octopus::BasicProjectileAttack<CustomProj>>({20./TICK_RATE})
+			.set<octopus::BasicProjectileAttack<CustomBasicProjectile>>({20./TICK_RATE})
 			.set<octopus::AttackCommand>({flecs::entity()})
 			.set<octopus::Attack>({{TICK_RATE/4, TICK_RATE, 15, 5}})
 			.set<VatLibraryHandle>({2})
 			.add<Pickable>()
-			.set<ProjTrajectory>({1.5,1})
+			.set<ProjectileTrajectory>({1.5,1})
 		;
 
 		octopus::AttackCommand atk_l {flecs::entity(), {12,5}, true};
@@ -111,7 +131,7 @@ void AttackMoveDemoNode::setup(Dictionary const &meta_data, GameNode &game) {
 				.set<octopus::Attack>({{TICK_RATE/4, int32_t(1.5*TICK_RATE), 25, 1}})
 				.set<VatLibraryHandle>({3})
 				.add<Pickable>()
-				.set<ProjTrajectory>({1.5,1})
+				.set<ProjectileTrajectory>({1.5,1})
 			;
 
 			octopus::AttackCommand atk_l {flecs::entity(), {50,100}, true};
