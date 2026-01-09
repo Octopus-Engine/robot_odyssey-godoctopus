3baa342a968ee3096a0ab3708a09bd7345f2965b
Simon <xxx.yyy.com>
2026-01-09 13:49:55 +0100

    more types

--- src/godoctopus/components/types/Types.h
+++ src/godoctopus/components/types/Types.h
@@ -1,7 +1,14 @@
 #pragma once
 
-struct HealBot { static constexpr char const * const naming()  { return "HealBot"; } };
-struct BackcanonBot { static constexpr char const * const naming()  { return "backcanon_bot"; } };
+struct HealBot { static constexpr char const * const naming() { return "healbot"; } };
+struct BackcanonBot { static constexpr char const * const naming() { return "backcanon_bot"; } };
+struct RamBot { static constexpr char const * const naming() { return "rambot"; } };
+struct TallBot { static constexpr char const * const naming() { return "tallbot"; } };
+struct EarBot { static constexpr char const * const naming() { return "earbot"; } };
+struct EarlargeBot { static constexpr char const * const naming() { return "earlarge_bot"; } };
+struct HighEbot { static constexpr char const * const naming() { return "high_ebot"; } };
+struct SmallroundEbot { static constexpr char const * const naming() { return "smallround_ebot"; } };
+struct TinyEbot { static constexpr char const * const naming() { return "tiny_ebot"; } };
 
 template<typename Callable, typename... Ts>
 void for_each_type(Callable&& callable) {
@@ -10,5 +17,15 @@ void for_each_type(Callable&& callable) {
 
 template<typename Callable>
 void for_each_bot_type(Callable&& callable) {
-	for_each_type<Callable, HealBot, BackcanonBot>(std::move(callable));
+	for_each_type<Callable
+		, HealBot
+		, BackcanonBot
+		, RamBot
+		, TallBot
+		, EarBot
+		, EarlargeBot
+		, HighEbot
+		, SmallroundEbot
+		, TinyEbot
+	>(std::move(callable));
 }
