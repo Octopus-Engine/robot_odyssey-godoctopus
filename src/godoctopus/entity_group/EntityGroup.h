79065497be54b2783b173f5163249dc440a50a5d
Simon <xxx.yyy.com>
2026-01-12 23:51:06 +0100

    added filter units group and remove dead entities

--- src/godoctopus/entity_group/EntityGroup.h
+++ src/godoctopus/entity_group/EntityGroup.h
@@ -13,15 +13,27 @@ class EntityGroup : public Resource {
 public:
 	static void _bind_methods() {
 		ClassDB::bind_method(D_METHOD("size"), &EntityGroup::size);
+		ClassDB::bind_method(D_METHOD("remove_dead_entities"), &EntityGroup::remove_dead_entities);
 	}
 
 	std::vector<flecs::entity> const & get_entities() const { return entities; }
 	std::vector<flecs::entity> & get_entities() { return entities; }
 
 	int size() const { return entities.size(); }
+	void remove_dead_entities() {
+		filter_group([](flecs::entity e) {
+			return !e.is_valid() || !e.is_alive() || !e.enabled();
+		});
+	}
 
 	// remove all entities that math to true to the filter
-	void filter_group(std::function<bool(flecs::entity)> filter);
+	void filter_group(std::function<bool(flecs::entity)> filter) {
+		entities.erase(std::remove_if(entities.begin(), entities.end(),
+			[&filter](flecs::entity e) {
+				return filter(e);
+			}
+		), entities.end());
+	}
 protected:
 private:
 	std::vector<flecs::entity> entities;
