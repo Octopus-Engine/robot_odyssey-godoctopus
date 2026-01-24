e9409f9c6efc24040f66b80bd655eb6c3bd9892e
SimonP <SimonP@gmail.com>
2026-01-25 00:23:23 +0100

    fixed bot types

--- src/godoctopus/components/types/Types.h
+++ src/godoctopus/components/types/Types.h
@@ -1,14 +1,18 @@
 #pragma once
 
-struct HealBot { static constexpr char const * const naming() { return "healbot"; } };
+struct ArmorBot { static constexpr char const * const naming() { return "armorbot"; } };
 struct BackcanonBot { static constexpr char const * const naming() { return "backcanon_bot"; } };
-struct RamBot { static constexpr char const * const naming() { return "rambot"; } };
-struct TallBot { static constexpr char const * const naming() { return "tallbot"; } };
+struct BladeBot { static constexpr char const * const naming() { return "bladebot"; } };
 struct EarBot { static constexpr char const * const naming() { return "earbot"; } };
 struct EarlargeBot { static constexpr char const * const naming() { return "earlarge_bot"; } };
-struct HighEbot { static constexpr char const * const naming() { return "high_ebot"; } };
-struct SmallroundEbot { static constexpr char const * const naming() { return "smallround_ebot"; } };
-struct TinyEbot { static constexpr char const * const naming() { return "tiny_ebot"; } };
+struct GunBot { static constexpr char const * const naming() { return "gunbot"; } };
+struct HealBot { static constexpr char const * const naming() { return "healbot"; } };
+struct High_eBot { static constexpr char const * const naming() { return "high_ebot"; } };
+struct RamBot { static constexpr char const * const naming() { return "rambot"; } };
+struct Smallround_eBot { static constexpr char const * const naming() { return "smallround_ebot"; } };
+struct TallBot { static constexpr char const * const naming() { return "tallbot"; } };
+struct TankyBot { static constexpr char const * const naming() { return "tankybot"; } };
+struct Tiny_eBot { static constexpr char const * const naming() { return "tiny_ebot"; } };
 
 template<typename Callable, typename... Ts>
 void for_each_type(Callable&& callable) {
@@ -18,14 +22,18 @@ void for_each_type(Callable&& callable) {
 template<typename Callable>
 void for_each_bot_type(Callable&& callable) {
 	for_each_type<Callable
-		, HealBot
-		, BackcanonBot
-		, RamBot
-		, TallBot
-		, EarBot
-		, EarlargeBot
-		, HighEbot
-		, SmallroundEbot
-		, TinyEbot
+		// , ArmorBot
+		// , BackcanonBot
+		// , BladeBot
+		// , EarBot
+		// , EarlargeBot
+		, GunBot
+		// , HealBot
+		// , High_eBot
+		// , RamBot
+		// , Smallround_eBot
+		// , TallBot
+		// , TankyBot
+		// , Tiny_eBot
 	>(std::move(callable));
 }
