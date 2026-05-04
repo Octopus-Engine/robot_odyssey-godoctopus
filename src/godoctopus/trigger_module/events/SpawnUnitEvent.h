de11222374ca64eed3e2c3e2114d3221c3f10741
SimonP <SimonP@gmail.com>
2026-05-04 10:49:17 +0200

    Improved clone respawn rune and test

--- src/godoctopus/trigger_module/events/SpawnUnitEvent.h
+++ src/godoctopus/trigger_module/events/SpawnUnitEvent.h
@@ -4,6 +4,7 @@
 
 #include "octopus/components/basic/player/Team.hh"
 #include "octopus/components/basic/position/Position.hh"
+#include "octopus/components/basic/hitpoint/HitPointMax.hh"
 #include "octopus/world/player/PlayerInfo.hh"
 #include "octopus/world/step/StepEntityManager.hh"
 #include "godoctopus/components/types/Types.h"
@@ -19,12 +20,14 @@ struct SpawnCopyEntityEvent {
 
 		octopus::Position pos = source.get<octopus::Position>();
 		octopus::Team team = source.get<octopus::Team>();
+		octopus::HitPointMax hpMax = source.get<octopus::HitPointMax>();
 		std::string type = source.get<PrefabType>().name;
 
 		octopus::EntityCreationStep step_l;
-		step_l.set_up_function = [pos, team, type](flecs::entity new_ent, flecs::world const &world_p) {
+		step_l.set_up_function = [pos, team, type, hpMax](flecs::entity new_ent, flecs::world const &world_p) {
 			new_ent.set<octopus::Position>(pos)
 				.is_a(world_p.prefab(type.c_str()))
+				.set<octopus::HitPoint>({hpMax.qty/2})
 				.set<octopus::Team>(team)
 				.set<octopus::PlayerAppartenance>({(uint32_t)team.team})
 				.add<ComponentEvent>()
@@ -32,35 +35,5 @@ struct SpawnCopyEntityEvent {
 		};
 
 		ecs.try_get_mut<octopus::StepEntityManager>()->get_last_layer().push_back(step_l);
-
-		// source.world().event<SpawnCopyEntityEvent<Component>>()
-		// 		.id<ComponentEvent>()
-		// 		.entity(source)
-		// 		.emit();
 	}
 };
-
-// void declare_spawn_copy_event_system(flecs::world &ecs, custom_step_manager &manager) {
-
-
-// 	ecs.observer<Rune const, typename Trigger::component const>()
-// 		.template event<Trigger>()
-// 		.each([&ctx](flecs::entity e, Rune const &rune, typename Trigger::component const &) {
-// 			octopus::Position const *pos = e.try_get<octopus::Position>();
-// 			octopus::Team const *team = e.try_get<octopus::Team>();
-// 			if(pos && team && Condition::check(e))
-// 			{
-// 				Event::apply(e, pos->pos, team->team, ctx, rune.level);
-// 			}
-// 		});
-
-// 	octopus::EntityCreationStep step_l;
-// 	step_l.set_up_function = [ecs, pos_l, type, spawn_action](flecs::entity new_ent, flecs::world const &world_p) {
-// 		new_ent.set<octopus::Position>(pos_l)
-// 			.is_a(ecs.prefab(type.c_str()))
-// 			.set<octopus::Team>({(uint16_t)spawn_action.team})
-// 			.set<octopus::PlayerAppartenance>({(uint32_t)spawn_action.team})
-// 			.set<custom_queue>(queue_l)
-// 		;
-// 	};
-// }
--- src/testing/SpawnCloneUnitRune.test.cpp
+++ src/testing/SpawnCloneUnitRune.test.cpp
@@ -38,6 +38,7 @@ struct GameNodeTestContextWithCustomPrefab {
 void test_gamenode_spawn_clone_unit_rune() {
 	// Create a unit prefab for testing
 	auto prefab = Ref<godot::UnitPrefab>(memnew(godot::UnitPrefab));
+	prefab->set_hitpoint(100);  // Max HP
 	prefab->set_prefab_name("rambot");
 
 	GameNodeTestContextWithCustomPrefab context(prefab);
@@ -64,12 +65,28 @@ void test_gamenode_spawn_clone_unit_rune() {
 	auto group = context.proxy_node->get_proxy_from_group(group_high_hp);
 	CHECK(group.size() == 0);
 
-	auto locker = context.proxy_node->get_data_locker();
-	auto const &proxy_map = locker.proxy_map;
-	CHECK(proxy_map.size() == 1);
+	flecs::entity new_unit;
+	{
+		auto locker = context.proxy_node->get_data_locker();
+		auto const &proxy_map = locker.proxy_map;
+		CHECK(proxy_map.size() == 1);
 
-	// Check that a new unit has been spawned
-	for (auto const &[entity_id, proxy_data] : proxy_map) {
-		CHECK(proxy_data.get_hp() > 0);
+		// Check that a new unit has been spawned
+		for (auto const &[entity_id, proxy_data] : proxy_map) {
+			CHECK(proxy_data.get_hp() == 50);
+			new_unit = proxy_data.entity;
+		}
+	}
+
+	// Set hp to 0 to ensure no other unit is spawned
+	new_unit.set<octopus::HitPoint>({0});
+	context.game_node->tick();
+
+	CHECK(new_unit.enabled() == false);
+
+	{
+		auto locker = context.proxy_node->get_data_locker();
+		auto const &proxy_map = locker.proxy_map;
+		CHECK(proxy_map.size() == 0);
 	}
 }
