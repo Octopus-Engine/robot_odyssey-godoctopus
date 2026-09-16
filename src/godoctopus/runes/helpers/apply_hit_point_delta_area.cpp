c879935d90085587992c5988b8d81f31f7cb664a
Simon <xxx.yyy.com>
2026-09-16 22:12:43 +0200

    added damage text on hit point delta

--- src/godoctopus/runes/helpers/apply_hit_point_delta_area.cpp
+++ src/godoctopus/runes/helpers/apply_hit_point_delta_area.cpp
@@ -1,5 +1,7 @@
 #include "apply_hit_point_delta_area.h"
 
+#include "godoctopus/on_screen_ui/DamageTextNode.h"
+
 void apply_hit_point_delta_area(flecs::entity source, octopus::Vector const &center, uint16_t team, octopus::PositionContext const &ctx,
 	octopus::Fixed range, octopus::Fixed delta)
 {
@@ -9,10 +11,10 @@ void apply_hit_point_delta_area(flecs::entity source, octopus::Vector const &cen
 		const bool check_team = ent.try_get<Team>() && (delta > 0
 			? ent.try_get<Team>()->team == team
 			: ent.try_get<Team>()->team != team);
-		if(check_team && ent.try_get<HitPoint>())
-		{
+		if(check_team && ent.try_get<HitPoint>()) {
 			HitPoint * hp = ent.try_get_mut<HitPoint>();
 			hp->qty += delta;
+			trigger_module::emit_damage_text_trigger(ent, std::abs(delta), delta > 0);
 		}
 		return true;
 	};
